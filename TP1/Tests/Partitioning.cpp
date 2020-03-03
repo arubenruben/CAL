44c81e7aa2cf1d5248d9ba7f9e607fcb07f4f25c
Ruben Almeida <almeidaruben35@gmail.com>
2020-03-03 17:07:44 +0000

    Recursive Bell number done

--- TP1/Tests/Partitioning.cpp
+++ TP1/Tests/Partitioning.cpp
@@ -4,10 +4,15 @@
 
 #include "Partitioning.h"
 
-
+//S(n,k) = S(n-1,k-1) + k S(n-1,k), se 1 < k < n
+//S(n,k) = 1, se k=1 ou k=n
 int s_recursive(int n,int k)
 {
-   return 1;
+    if(k==1||n==1||k==n)
+        return 1;
+    else if(k>1 && k<n){
+        return  s_recursive(n-1,k-1)+k*s_recursive(n-1,k);
+    }
 }
 
 int s_dynamic(int n,int k)
@@ -18,7 +23,12 @@ int s_dynamic(int n,int k)
 
 int b_recursive(int n)
 {
-	return 0;
+    int sum=0;
+
+    for(int k=1;k<=n;k++){
+        sum+=s_recursive(n,k);
+    }
+	return sum;
 }
 
 int b_dynamic(int n)
